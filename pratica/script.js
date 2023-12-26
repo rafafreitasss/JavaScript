3f0de90ad967e1b01a70971713e420e1fb074370
rafafreitasss <rafaelfreitas97@yahoo.com>
2023-12-26 15:45:03 -0300

    atualização

--- pratica/script.js
+++ pratica/script.js
@@ -1,13 +1,10 @@
 function verificar() {
     var ano = window.document.getElementById('ano')
     var res = window.document.getElementById('res')
+    var sexo = document.getElementsByName('sexo')
     if (ano.value.length == 0 || ano.value > 2023) {
         window.alert('Verifique os dados e tente novamente!')
-    } else {
-        var sexo = document.getElementsByName('sexo')
-        
-        if (ano == 1975 && sexo == fem) {
-            res.innerHTML = 'Você é a Patrícia!'
-        }
+    } else if (ano == 1975 && sexo == fem) {
+        res.innerHTML = 'Você é a Patrícia!'
     }
 }
